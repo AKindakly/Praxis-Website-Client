df6ddb0bdd0abe3a4223b3783ac6764192f7c8e5
Akindakly <107426060+AKindakly@users.noreply.github.com>
2023-05-14 20:28:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
-// import Admin from "./components/Admin/Admin";
 import About from "./components/About/About";
 import OurServices from "./components/OurServices/OurServices";
-// import Dashboard from "./components/Admin/Dashboard/Dashboard";
 import Contact from "./components/Contact/Contact";
-// import EmployeesBoard from "./components/Admin/Dashboard/EmployeesBoard/EmployeesBoard";
 
 const App = () => {
     return (
@@ -15,10 +12,6 @@ const App = () => {
             <Route exact path="/about" element={<About />} />
             <Route exact path="/services" element={<OurServices />} />
             <Route exact path="/contact" element={<Contact />} />
-            {/* 
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route exact path="/dashboard1" element={<EmployeesBoard />} /> */}
         </Routes>
     );
 };
